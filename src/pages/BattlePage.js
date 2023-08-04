336aca6319141d58c735aa0057359bc981463454
godley0623 <godley11691@yahoo.com>
2023-08-04 02:08:27 -0400

    Added pokemon sprites to battle bg

--- src/pages/BattlePage.js
+++ src/pages/BattlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { setPageBackground } from '../controller/controller'
 import '../styles/battlePage.css'
+import moveJson from '../data/moves.json';
 
 import grassBg from '../assets/pokemonBWBG/battle_bg_grass.png'
 import indoorBg from '../assets/pokemonBWBG/battle_bg_indoor.png'
@@ -14,6 +15,7 @@ export default function BattlePage() {
     const [wildPkmn, setWildPkmn] = useState({});
     const [titleText, setTitleText] = useState('');
     const [trainer, setTrainer] = useState('');
+    const [pkmnParty, setPkmnParty] = useState([]);
 
     const bgStyle = {
         backgroundImage: `url(${grassBg})`,
@@ -22,6 +24,11 @@ export default function BattlePage() {
 
     
     useEffect(() => {
+        if (param != 'wild') navigate('/play')
+
+        const lsParty = JSON.parse(localStorage.getItem('PSV: pkmn-party'))
+        setPkmnParty(lsParty);
+        
         if (!localStorage.getItem('PSV: wild-pkmn') && param === 'wild') {
             navigate('/play');
         } else {
@@ -34,6 +41,7 @@ export default function BattlePage() {
     useEffect(() => {
         setTitleText(`A wild ${wildPkmn['name']} appeared!`)
         if (wildPkmn['sprite']) console.log(wildPkmn['sprite'][0])
+        console.log(pkmnParty)
     }, [wildPkmn])
 
   return (
@@ -42,11 +50,35 @@ export default function BattlePage() {
             {titleText}
             <div className='bg-container'>
                 <div className='bg' style={bgStyle}>
-                    {wildPkmn['sprite'] && 
-                    <img src={wildPkmn['sprite'][0]} alt='wild pokemon sprite'/>}
+                    {wildPkmn['sprite'] &&
+                    <div className='opp-field'>
+                        <img src={wildPkmn['sprite'][0]} alt='opponent pokemon sprite' />
+                    </div>
+                    }
+                    {pkmnParty[0] &&
+                    <div className='your-field'>
+                        <img src={pkmnParty[0]['sprite'][1]} alt='your pokemon sprite' />
+                    </div>
+                    }
                 </div>
                 {param !== 'wild' &&
                 <img src={trainer} alt='trainer sprite' />
+                }
+            </div>
+            <div className='move-container'>
+                {pkmnParty[0] &&
+                pkmnParty[0]['moves'].map((move, key) => (
+                    <div key={key} className={`move ${moveJson[move]['type']}`}>{move}</div>
+                ))
+                }
+            </div>
+            <div className='battle-party-container'>
+                {pkmnParty &&
+                pkmnParty.map((pkmn, key) => (
+                   <div key={key} className='icon-container'>
+                        <img src={pkmn['icon']} alt='pokemon icon'/>
+                   </div> 
+                ))
                 }
             </div>
         </div>
