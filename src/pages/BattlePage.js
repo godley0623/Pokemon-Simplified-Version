58df00a2df737163c6d83772b2b8958a6d72b371
godley0623 <godley11691@yahoo.com>
2023-08-04 20:26:55 -0400

    add health bars

--- src/pages/BattlePage.js
+++ src/pages/BattlePage.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { setPageBackground } from '../controller/controller'
+import { setPageBackground, capFirstLetter } from '../controller/controller'
 import '../styles/battlePage.css'
 import moveJson from '../data/moves.json';
 
 import grassBg from '../assets/pokemonBWBG/battle_bg_grass.png'
 import indoorBg from '../assets/pokemonBWBG/battle_bg_indoor.png'
+
+// import { HealthBar } from '../classes/healthBar';
+// const yourHp = new HealthBar(300);
+// const oppHp = new HealthBar(250);
+
+
 
 export default function BattlePage() {
     setPageBackground('');
@@ -16,6 +22,12 @@ export default function BattlePage() {
     const [titleText, setTitleText] = useState('');
     const [trainer, setTrainer] = useState('');
     const [pkmnParty, setPkmnParty] = useState([]);
+    const [yourHp, setYourHp] = useState([100, 100]);
+    const [oppHp, setOppHp] = useState([100, 100]);
+    const [yourFillStyle, setYourFillStyle] = useState({});
+    const [oppFillStyle, setOppFillStyle] = useState({});
+    const [yourFillColor, setYourFillColor] = useState('rgb(2,203,88)');
+    const [oppFillColor, setOppFillColor] = useState('rgb(2,203,88)');
 
     const bgStyle = {
         backgroundImage: `url(${grassBg})`,
@@ -24,7 +36,7 @@ export default function BattlePage() {
 
     
     useEffect(() => {
-        if (param != 'wild') navigate('/play')
+        if (param !== 'wild') navigate('/play')
 
         const lsParty = JSON.parse(localStorage.getItem('PSV: pkmn-party'))
         setPkmnParty(lsParty);
@@ -35,20 +47,85 @@ export default function BattlePage() {
             setWildPkmn(JSON.parse(localStorage.getItem('PSV: wild-pkmn')))
             localStorage.removeItem('PSV: wild-pkmn');
         }
-        
     }, [])
 
     useEffect(() => {
-        setTitleText(`A wild ${wildPkmn['name']} appeared!`)
-        if (wildPkmn['sprite']) console.log(wildPkmn['sprite'][0])
-        console.log(pkmnParty)
+        if (wildPkmn['name']) {
+            setTitleText(`A wild ${capFirstLetter(wildPkmn['name'])} appeared!`)
+        }
+        setOppHp([wildPkmn['hp'], wildPkmn['currentHp']])
     }, [wildPkmn])
+
+    useEffect(() => {
+        if (pkmnParty[0]) setYourHp([pkmnParty[0]['hp'], pkmnParty[0]['currentHp']])
+        
+    }, [pkmnParty])
+
+    useEffect(() => {
+        const hp = Math.floor(yourHp[1] / yourHp[0] * 100);
+        console.log(hp)
+        if (hp > 49) {
+            setYourFillColor('rgb(2,203,88)');
+        } else if (hp > 20 && hp <= 49) {
+            console.log(1)
+            setYourFillColor('rgb(245,213,55)');
+        } else {
+            setYourFillColor('rgb(238,74,39)');
+        }
+        setYourFillStyle({
+            width: `${hp}%`,
+            background: yourFillColor,
+        })
+    }, [yourHp, yourFillColor])
+
+    useEffect(() => {
+        const hp = Math.floor(oppHp[1] / oppHp[0] * 100);
+        console.log(hp)
+        if (hp > 49) {
+            setOppFillColor('rgb(2,203,88)');
+        } else if (hp > 20 && hp <= 49) {
+            console.log(1)
+            setOppFillColor('rgb(245,213,55)');
+        } else {
+            setOppFillColor('rgb(238,74,39)');
+        }
+        setOppFillStyle({
+            width: `${hp}%`,
+            background: oppFillColor,
+        })
+    }, [oppHp, oppFillColor])
+
+    function handleMoveClick() {
+        setOppHp([oppHp[0], oppHp[1] - 5]);
+    }
 
   return (
     <div className='battle-page' >
         <div className='battle-container'>
-            {titleText}
+            {`${titleText}`}
             <div className='bg-container'>
+                <div className='your-hp-container'>
+                    {pkmnParty[0] &&
+                    <>
+                        <h3>{capFirstLetter(pkmnParty[0]['name'])}</h3>
+                        <div className='hp-bar'>
+                            <p className='hp-percent'>{`${Math.floor(yourHp[1] / yourHp[0] * 100)}%`}</p>
+                            <div className='fill' style={yourFillStyle}></div>
+                        </div>
+                    </>
+                    }
+                </div>
+                <div className='opp-hp-container'>
+                    {wildPkmn['name'] &&
+                    <>
+                        <h3>{capFirstLetter(wildPkmn['name'])}</h3>
+                        <div className='hp-bar'>
+                            <p className='hp-percent'>{`${Math.floor(oppHp[1] / oppHp[0] * 100)}%`}</p>
+                            <div className='fill' style={oppFillStyle}></div>
+                        </div>
+                    </>
+                    }
+                </div>
                 <div className='bg' style={bgStyle}>
                     {wildPkmn['sprite'] &&
                     <div className='opp-field'>
@@ -68,7 +145,7 @@ export default function BattlePage() {
             <div className='move-container'>
                 {pkmnParty[0] &&
                 pkmnParty[0]['moves'].map((move, key) => (
-                    <div key={key} className={`move ${moveJson[move]['type']}`}>{move}</div>
+                    <div onClick={handleMoveClick} key={key} className={`move ${moveJson[move]['type']}`}>{move}</div>
                 ))
                 }
             </div>
