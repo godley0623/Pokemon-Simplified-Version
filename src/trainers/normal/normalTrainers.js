a3ad82bc61bfe43045929cf5eab81f4d76904e81
godley0623 <godley11691@yahoo.com>
2023-08-18 21:20:13 -0400

    Added sei424 Alison & Kevin

--- src/trainers/normal/normalTrainers.js
+++ src/trainers/normal/normalTrainers.js
@@ -1,3 +1,4 @@
+import { choose } from '../../controller/controller'
 import { Trainer } from "../trainer"
 import youngster from '../../assets/trainerSprites/Spr_B2W2_Youngster.png'
 import pokefanM from '../../assets/trainerSprites/Spr_B2W2_Pokéfan_M.png'
@@ -6,6 +7,8 @@ import backpackerF from '../../assets/trainerSprites/Spr_B2W2_Backpacker_F.png'
 import clerkF from '../../assets/trainerSprites/Spr_B2W2_Clerk_F.png'
 import breederM from '../../assets/trainerSprites/Spr_B2W2_Pokémon_Breeder_M.png'
 import joker from '../../assets/trainerSprites/Spr_B2W2_Harlequin.png'
+import lady from '../../assets/trainerSprites/Spr_B2W2_Lady.png'
+import striker from '../../assets/trainerSprites/Spr_B2W2_Striker.png'
 
 export const youngsterJoey2 = new Trainer('Youngster', 'Joey', 'Normal', youngster, 'trainer', [
     {
@@ -181,11 +184,87 @@ export const sei424Grant = new Trainer('SEI424', 'Grant', 'Normal', joker, 'sei4
     }
 ])
 
+const astroChoice = choose(['lunatone', 'solrock'])
+let astro;
+if (astroChoice === 'lunatone') {
+    astro = {
+        'pokemon': astroChoice,
+        'moves': ['Rock+', 'Psychic+', 'Flying', 'Water+']
+    }
+} else {
+    astro = {
+        'pokemon': astroChoice,
+        'moves': ['Rock+', 'Psychic+', 'Flying', 'Fire+']
+    }
+}
+//console.log(astro)
+export const sei424Alison = new Trainer('SEI424', 'Alison', 'Normal', lady, 'sei424', [
+    {
+        'pokemon': astro['pokemon'],
+        'moves': astro['moves'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'lopunny',
+        'moves': ['Normal+', 'Fairy+', 'Fighting'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'bellossom',
+        'moves': ['Grass+', 'Fairy+', 'Rock',],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'milotic',
+        'moves': ['Water+', 'Fairy+', 'Ground+', 'Ice+'],
+        'items': [],
+        'ai': ['weakness']
+    }
+])
+
+export const sei424Kevin = new Trainer('SEI424', 'Kevin', 'Normal', striker, 'sei424', [
+    {
+        'pokemon': 'hitmontop',
+        'moves': ['Fighting+', 'Dark', 'Ice'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'electrode',
+        'moves': ['Electric+', 'Dark', 'Grass'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'hitmonlee',
+        'moves': ['Fighting+', 'Dark', 'Electric'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'bisharp',
+        'moves': ['Steel+', 'Dark+', 'Flying', 'Rock'],
+        'items': [],
+        'ai': ['weakness']
+    },
+    {
+        'pokemon': 'druddigon',
+        'moves': ['Dragon+', 'Dark+', 'Steel+', 'Grass+'],
+        'items': [],
+        'ai': ['weakness']
+    }
+])
+
 export const normalTrainers = [
     youngsterJoey2,
     sei424Jamari,
     sei424Larisa,
     sei424Luke,
     sei424Devin,
-    sei424Grant
+    sei424Grant,
+    sei424Alison,
+    sei424Kevin
 ]
