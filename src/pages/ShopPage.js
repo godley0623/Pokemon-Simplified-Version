a0b56f038ce7c1540ac8776eb92481d1512ddfa3
godley0623 <godley11691@yahoo.com>
2023-08-21 12:33:06 -0400

    updated shop data

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -13,7 +13,18 @@ import leftovers from '../assets/itemSprites/leftovers.png'
 import sitrusBerry from '../assets/itemSprites/sitrus-berry.png'
 import normalMove from '../assets/itemSprites/normal-move.png'
 import grassMove from '../assets/itemSprites/grass-move.png'
+import fireMove from '../assets/itemSprites/fire-move.png'
 import waterMove from '../assets/itemSprites/water-move.png'
+import electricMove from '../assets/itemSprites/electric-move.png'
+import rockMove from '../assets/itemSprites/rock-move.png'
+import flyingMove from '../assets/itemSprites/flying-move.png'
+import pyschicMove from '../assets/itemSprites/pyschic-move.png'
+import bugMove from '../assets/itemSprites/bug-move.png'
+import darkMove from '../assets/itemSprites/dark-move.png'
+import dragonMove from '../assets/itemSprites/dragon-move.png'
+import ghostMove from '../assets/itemSprites/ghost-move.png'
+import iceMove from '../assets/itemSprites/ice-move.png'
+import steelMove from '../assets/itemSprites/steel-move.png'
 
 const itemObj = {
     'evolution-stone.png': evolutionStone,
@@ -23,7 +34,18 @@ const itemObj = {
     'sitrus-berry.png': sitrusBerry,
     'normal-move.png': normalMove,
     'grass-move.png': grassMove,
-    'water-move.png': waterMove
+    'water-move.png': waterMove,
+    'fire-move.png': fireMove,
+    'electric-move.png': electricMove,
+    'rock-move.png': rockMove,
+    'steel-move.png': steelMove,
+    'flying-move.png': flyingMove,
+    'bug-move.png': bugMove,
+    'pyschic-move.png': pyschicMove,
+    'ghost-move.png': ghostMove,
+    'dark-move.png': darkMove,
+    'dragon-move.png': dragonMove,
+    'ice-move.png': iceMove
 }
 
 const items = shopItems['shop']
